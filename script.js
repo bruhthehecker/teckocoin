6c34b14299a6a7ea464a5184337d4b5cdd58fe94
bruhthehecker <164286233+bruhthehecker@users.noreply.github.com>
2024-05-27 14:56:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', function() {
 
   // checking whether fully visible
   if(position.top >= 0 && position.bottom <= window.innerHeight) {
-    element.classList.add('text-pop-up-top', 'tilt-in-bottom-2', 'class3'); // Add more classes as needed
+    element.classList.add('text-pop-up-top', 'tilt-in-bottom-2', 'nav-animate'); // Add more classes as needed
   } else {
     element.classList.remove('text-pop-up-top', 'tilt-in-bottom-2', 'nav-animate'); // Remove the same classes
   }
